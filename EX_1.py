ecb6fde252d1a999869256898608e20471f542e1
Rafael Vieira <rafavs09@gmail.com>
2024-04-02 16:03:21 -0300

    Delete a extra file

--- EX_1.py
+++
@@ -1,9 +0,0 @@
-#Valores propostos pelo usuario
-peso = float(input("Qual o seu peso: "))
-altura = float(input("Qual a sua altura: "))
-
-#Calculo do imc
-resultado = peso/altura*altura
-
-#Resultado do Calculo
-print("Seu IMC é:", resultado)
