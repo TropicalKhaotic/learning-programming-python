fc55ad487f19a90c5298adf8b3c17582ee4af2f7
Rafael Vieira <rafavs09@gmail.com>
2024-04-02 17:06:49 -0300

    cylinder volume calculator
    --Use this if you ever need to calculate a cylinder volume.

--- EX_3.py
+++ EX_3.py
@@ -1,10 +1,17 @@
-#Valores propostos pelo usuario
-peso = float(input("Qual o seu peso: "))
-altura = float(input("Qual a sua altura: "))
+#Calculadora para determinar o volume de um cilindro
+import math
 
-#Calculo do imc
-resultado = peso/altura*altura
+print("Calculadora de volume de um cilindro")
+operação = (input("Deseja calcular o volume de um cilindro (Y/N):"))
+operação = operação.lower()
+if operação == "y":
+   raio = float(input("Qual o valor do raio:"))
+   altura = float(input("Qual o valor da altura:"))
+   resultado = (math.pi*(raio*raio))*altura
+   resultado = round(resultado,2)
+   print("O valor do volume é:",resultado)
 
-#Resultado do Calculo
-print("Seu IMC é:", resultado)
-
+elif operação == "n":
+   print("Acontece")
+else:
+   print("Blz ent")
