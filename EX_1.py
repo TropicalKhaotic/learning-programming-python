8aed0c9c3b5e9191c1b6d77f0f2dc5bd25446bff
Rafael Vieira <rafavs09@gmail.com>
2024-04-02 14:46:31 -0300

    Calculator IMC in Python
    -Use this if need to calculate IMC

---
+++ EX_1.py
@@ -0,0 +1,10 @@
+#Valores propostos pelo usuario
+peso = float(input("Qual o seu peso: "))
+altura = float(input("Qual a sua altura: "))
+
+#Calculo do imc
+resultado = peso/altura*altura
+
+#Resultado do Calculo
+print("Seu IMC é:", resultado)
+
